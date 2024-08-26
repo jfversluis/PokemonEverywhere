c6372aa0b3098b8bbcb8148053591043c6a10b60
Gerald Versluis <gerald.versluis@microsoft.com>
2024-08-26 21:17:43 +0200

    Add Blazor Web

---
+++ PokemonEverywhere.Shared/Services/PokemonLocalService.cs
@@ -0,0 +1,22 @@
+﻿using PokemonEverywhere.Shared.Models;
+using System.Net.Http.Json;
+
+namespace PokemonEverywhere.Shared.Services;
+
+public class PokemonLocalService
+{
+    private readonly HttpClient _httpClient;
+
+    public PokemonLocalService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<List<Pokemon>> GetPokemonAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Pokemon>>("/pokemon") ?? [];
+    }
+    public async Task<Pokemon?> GetPokemonAsync(int id)
+    {
+        return (await GetPokemonAsync()).FirstOrDefault(p => p.Id == id);
+    }
+}
