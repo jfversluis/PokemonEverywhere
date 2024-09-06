6aadfc493fc61c22cd0301094754ab67e9a87956
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-06 14:12:19 +0200

    Add WPF

---
+++ PokemonEverywhere.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using PokemonEverywhere.Shared.Models;
+using PokemonEverywhere.Shared.Services;
+
+namespace PokemonEverywhere.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private readonly ServiceProvider serviceProvider;
+
+    public ObservableCollection<Pokemon> Pokemon { get; set; } = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<PokemonLocalService>(
+            sp => new PokemonLocalService(
+                new System.Net.Http.HttpClient()
+                {
+                    BaseAddress = new Uri("https://localhost:7055/")
+                }));
+
+        serviceCollection.AddWpfBlazorWebView();
+
+        serviceProvider = serviceCollection.BuildServiceProvider();
+
+        Resources.Add("services", serviceProvider);
+
+        DataContext = this;
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        var pokemon = serviceProvider.GetRequiredService<PokemonLocalService>();
+
+        foreach (var p in await pokemon.GetPokemonAsync())
+        {
+            Pokemon.Add(p);
+        }
+    }
+}
