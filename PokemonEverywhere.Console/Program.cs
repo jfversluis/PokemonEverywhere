2ef9fcc71a2b338b27b1b651c84cb474ee8a2c94
Gerald Versluis <gerald.versluis@microsoft.com>
2024-09-06 14:59:15 +0200

    Added Console

---
+++ PokemonEverywhere.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleTables;
+using PokemonEverywhere.Shared.Services;
+
+var pokemonService = new PokemonLocalService(
+    new HttpClient()
+    {
+        BaseAddress = new Uri("https://localhost:7055/")
+    });
+
+var table = new ConsoleTable("ID", "Name", "Species");
+
+foreach (var pokemon in await pokemonService.GetPokemonAsync())
+{
+    table.AddRow(pokemon.Id, pokemon.Name, pokemon.Species);
+}
+
+table.Write();
+
+Console.ReadLine();
